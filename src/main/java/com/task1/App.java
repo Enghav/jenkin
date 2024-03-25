c5e8a81d48b80ba686c74ddf97a79e51e785a372
Enghav <128684916+Enghav@users.noreply.github.com>
2024-03-25 14:17:47 +0700

    test

---
+++ src/main/java/com/task1/App.java
@@ -0,0 +1,14 @@
+package com.task1;
+import javax.swing.JFrame;
+/**
+ * Hello World!
+ * 
+ */
+public class App {
+    public static void main (String [] args){
+        JFrame text =new JFrame("Hello World!");
+        text.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        text.setSize(400,100);
+        text.setVisible(true);
+    }
+}
